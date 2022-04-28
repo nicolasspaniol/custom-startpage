ea029ebc0b3f2a0ba93e9a116bb764b1eb4a0a73
Nicolas Spaniol <nicolasmspaniol@gmail.com>
2022-04-27 21:00:39 -0300

    made bookmarks as auto complete suggestions

--- js/region/search.js
+++ js/region/search.js
@@ -2,5 +2,49 @@
 
 const searchIcon = document.getElementById("search-icon");
 const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search");
+const searchSuggestions = document.getElementById("search-suggestions");
 
 searchIcon.onclick = () => searchForm.submit();
+
+// Bookmarks auto complete
+
+let nameTable = {};
+let links = [];
+
+const BOOKMARKS_SRC = "/data/sites.txt";
+
+const bookmarks = await (await fetch(BOOKMARKS_SRC)).text();
+let i = 0;
+for (let bm of bookmarks.split("\n")) {
+  let [names, link] = bm.split("=");
+  names = names.split(",");
+  for (let name of names) {
+    nameTable[name] = i;
+  }
+  links.push([names[0], link]);
+  i++;
+}
+
+async function handleSearchInput() {
+  searchSuggestions.innerHTML = "";
+  if (searchInput.value == "") return;
+
+  const suggestionSet = new Set();
+
+  for (let [k, v] of Object.entries(nameTable)) {
+    if (k.startsWith(searchInput.value)) {
+      suggestionSet.add(links[v]);
+    }
+  }
+
+  for (let suggestion of suggestionSet) {
+    const elem = document.createElement("a");
+    elem.href = suggestion[1];
+    elem.innerHTML = suggestion[0];
+    elem.classList.add("suggestion");
+    searchSuggestions.append(elem);
+  }
+}
+
+searchInput.addEventListener("input", handleSearchInput);
