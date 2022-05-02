9a89d7b57a27b45dbef0cd336f57bc2ccc7c19f8
Nicolas Spaniol <nicolasmspaniol@gmail.com>
2022-05-02 17:19:06 -0300

    created "main.js" and added id function back

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import * as util from "/js/util.js";
+
+window.ids = util.getAllElementsWithId();
--- js/util.js
+++ js/util.js
@@ -9,3 +9,17 @@ export function addUrlParameters(url, params) {
 
   return url + "?" + paramArr.join("&");
 }
+
+export function getAllElementsWithId(parent = document) {
+  const ids = {};
+  for (let elem of parent.querySelectorAll("*[id]")) {
+    ids[toCamelCase(elem.id)] = elem;
+  }
+  return ids;
+}
+
+// Private
+
+function toCamelCase(varName) {
+  return varName.toLowerCase().replace(/[ -_](\w)/g, (_, g) => g[0].toUpperCase());
+}
